c2a2a14a25b780dbe02dbf4c5b3b205e9b4db62d
sewon <dltpdnjs2000@gmail.com>
2024-02-01 22:39:37 +0900

    dup commit test 3

--- src/main/java/com/example/demo/DupCommitTest.java
+++ src/main/java/com/example/demo/DupCommitTest.java
@@ -3,6 +3,7 @@ package com.example.demo;
 public class DupCommitTest {
     public static void main(String[] args) {
         System.out.println("dup commit test");
-        System.out.println("빈 브랜치에서 다른브랜치 커밋 squash and merge로 합침");
+        System.out.println("빈 브랜치에서 다른브랜치 커밋 squash and merge로 합침"+
+                " 대신에 모든 파일이 새로운 파일로 등록됨");
     }
 }
