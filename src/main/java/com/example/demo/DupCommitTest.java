6761b7d3f38b1340d951e81f2197415cfd04e223
sewon <dltpdnjs2000@gmail.com>
2024-02-01 22:38:38 +0900

    dup commit test 2

--- src/main/java/com/example/demo/DupCommitTest.java
+++ src/main/java/com/example/demo/DupCommitTest.java
@@ -3,5 +3,6 @@ package com.example.demo;
 public class DupCommitTest {
     public static void main(String[] args) {
         System.out.println("dup commit test");
+        System.out.println("빈 브랜치에서 다른브랜치 커밋 squash and merge로 합침");
     }
 }
